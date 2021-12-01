8581c95f25303b684bf68985a4389cb1d835cc73
Diogo Santos (CTW) <Diogo.JS.Santos@ctw.bmwgroup.com>
2021-12-01 12:11:00 +0000

    Setup for day2

--- app/src/main/kotlin/advent/of/code/AdventOfCodeSolutions.kt
+++ app/src/main/kotlin/advent/of/code/AdventOfCodeSolutions.kt
@@ -6,9 +6,24 @@ import java.io.File
 
 fun main() {
     Day1(File(getFilePath("input1.txt")).readLines().map { it.toInt() }).printSolutions()
+    Day2(File(getFilePath("input1.txt")).readLines().map { it.toInt() }).printSolutions()
 }
 
-class Day1(private val input: List<Int>): DaySolution {
+class Day2(private val input: List<Int>) : DaySolution {
+    override var day = 2
+    override fun getPart1Solution(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPart2Solution(): Any {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class Day1(private val input: List<Int>) : DaySolution {
+    override var day = 1
+
     override fun getPart1Solution() = input.windowed(2).count { it[1] > it[0] }
     override fun getPart2Solution() = input.windowed(3).map { it.sum() }.windowed(2).count { it[1] > it[0] }
 }
--- app/src/main/kotlin/advent/of/code/shared/DaySolution.kt
+++ app/src/main/kotlin/advent/of/code/shared/DaySolution.kt
@@ -1,20 +1,13 @@
 package advent.of.code.shared
 
 interface DaySolution {
+    var day: Int
     fun getPart1Solution(): Any
     fun getPart2Solution(): Any
 
     fun printSolutions() {
-        printSolution(1, this)
-        printSolution(2, this)
-    }
-
-    private fun printSolution(part: Int, solution: DaySolution) {
-        val result = when(part) {
-            1 -> solution.getPart1Solution()
-            2 -> solution.getPart2Solution()
-            else -> error("Invalid part")
-        }
-        println("Part $part solution: $result")
+        println("== Day $day solutions ==")
+        println("Part 1: ${getPart1Solution()}")
+        println("Part 2: ${getPart2Solution()}")
     }
 }
