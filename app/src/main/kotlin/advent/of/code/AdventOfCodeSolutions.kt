75dbfc077489bc5093de19a1334d73efd30a7755
Diogo Santos (CTW) <Diogo.JS.Santos@ctw.bmwgroup.com>
2021-12-14 19:10:48 +0000

    solution aggregate improvement

--- app/src/main/kotlin/advent/of/code/AdventOfCodeSolutions.kt
+++ app/src/main/kotlin/advent/of/code/AdventOfCodeSolutions.kt
@@ -3,6 +3,7 @@ package advent.of.code
 import advent.of.code.day1.Day1
 import advent.of.code.day10.Day10
 import advent.of.code.day11.Day11
+import advent.of.code.day12.Day12
 import advent.of.code.day13.Day13
 import advent.of.code.day14.Day14
 import advent.of.code.day2.Day2
@@ -17,19 +18,23 @@ import advent.of.code.shared.getFilePath
 import java.io.File
 
 fun main() {
-    Day1(File(getFilePath("input1.txt")).readLines()).printSolutionsAndTime()
-    Day2(File(getFilePath("input2.txt")).readLines()).printSolutionsAndTime()
-    Day3(File(getFilePath("input3.txt")).readLines()).printSolutionsAndTime()
-    Day4(File(getFilePath("input4.txt")).readLines()).printSolutionsAndTime()
-    Day5(File(getFilePath("input5.txt")).readLines()).printSolutionsAndTime()
-    Day6(File(getFilePath("input6.txt")).readText()).printSolutionsAndTime()
-    Day7(File(getFilePath("input7.txt")).readText()).printSolutionsAndTime()
-    Day8(File(getFilePath("input8.txt")).readLines()).printSolutionsAndTime()
-    Day9(File(getFilePath("input9.txt")).readLines()).printSolutionsAndTime()
-    Day10(File(getFilePath("input10.txt")).readLines()).printSolutionsAndTime()
-    Day11(File(getFilePath("input11.txt")).readLines()).printSolutionsAndTime()
-    // TODO: Day12
-    Day13(File(getFilePath("input13.txt")).readLines()).printSolutionsAndTime()
-    Day14(File(getFilePath("input14.txt")).readLines()).printSolutionsAndTime()
+    listOf(
+        Day1(File(getFilePath("input1.txt")).readLines()),
+        Day2(File(getFilePath("input2.txt")).readLines()),
+        Day3(File(getFilePath("input3.txt")).readLines()),
+        Day4(File(getFilePath("input4.txt")).readLines()),
+        Day5(File(getFilePath("input5.txt")).readLines()),
+        Day6(File(getFilePath("input6.txt")).readText()),
+        Day7(File(getFilePath("input7.txt")).readText()),
+        Day8(File(getFilePath("input8.txt")).readLines()),
+        Day9(File(getFilePath("input9.txt")).readLines()),
+        Day10(File(getFilePath("input10.txt")).readLines()),
+        Day11(File(getFilePath("input11.txt")).readLines()),
+        Day12(File(getFilePath("input12.txt")).readLines()),
+        Day13(File(getFilePath("input13.txt")).readLines()),
+        Day14(File(getFilePath("input14.txt")).readLines())
+    ).forEach {
+        it.printSolutionsAndTime()
+    }
 }
 
