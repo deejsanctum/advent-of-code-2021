90f02a945b0d387299cc8c1c6bbe4169bc0a1ddb
Diogo Santos (CTW) <Diogo.JS.Santos@ctw.bmwgroup.com>
2021-12-14 17:04:53 +0000

    day 9 solution (improvements and part 2)

--- app/src/main/kotlin/advent/of/code/day9/Day9.kt
+++ app/src/main/kotlin/advent/of/code/day9/Day9.kt
@@ -8,27 +8,58 @@ class Day9(val input: List<String>) : DaySolution {
     override var day = 9
     private val locations = calcLocations()
 
-    override fun getPart1Solution() = locations.flatten().filter { it.isLowPoint }.sumOf { it.riskLevel }
+    override fun getPart1Solution() = getLowPoints().sumOf { it.height + 1 }
 
-    override fun getPart2Solution(): Any {
-        return "Not implemented"
+    override fun getPart2Solution() = getLowPoints()
+        .map { getBasin(it).size }
+        .sortedDescending()
+        .subList(0, 3)
+        .reduce { acc, i -> acc * i }
+
+    private fun getLowPoints() = locations.flatten().filter { it.isLowPoint() }
+
+    private fun getBasin(initialLocation: Location): List<Location> {
+        val locations = mutableListOf(initialLocation)
+        var edges = listOf(initialLocation)
+        while (edges.isNotEmpty()) {
+            val newEdges = mutableListOf<Location>()
+            edges.forEach { edge ->
+                val adjacentLocations = edge.adjacentLocations.filter { !locations.contains(it) && it.height < 9 }
+                locations.addAll(adjacentLocations)
+                newEdges.addAll(adjacentLocations)
+            }
+            edges = newEdges
+        }
+        return locations
     }
 
     private fun calcLocations(): List<List<Location>> {
-        val heights = mutableListOf<List<Int>>()
-        input.forEach { line ->
-            heights.add(line.map { it.toString().toInt() })
+        val result = input.mapIndexed { y, line ->
+            line.mapIndexed { x, c -> Location(c.toString().toInt()) }
         }
-        return heights.mapIndexed { y, lines ->
-            lines.mapIndexed { x, _ ->
-                Location(heights, x, y)
+        val maxY = result.size - 1
+        val maxX = result[0].size - 1
+        result.forEachIndexed { y, row ->
+            row.forEachIndexed { x, location ->
+                if (y < maxY) {
+                    location.addAdjacentLocation(result[y + 1][x])
+                }
+                if (y > 0) {
+                    location.addAdjacentLocation(result[y - 1][x])
+                }
+                if (x > 0) {
+                    location.addAdjacentLocation(result[y][x - 1])
+                }
+                if (x < maxX) {
+                    location.addAdjacentLocation(result[y][x + 1])
+                }
             }
         }
+        return result.toList()
     }
 }
 
 fun main() {
     val input = File(getFilePath("input9.txt")).readLines()
-//    val input = listOf("2199943210", "3987894921", "9856789892", "8767896789", "9899965678")
     Day9(input).printSolutions()
 }
--- app/src/main/kotlin/advent/of/code/day9/Location.kt
+++ app/src/main/kotlin/advent/of/code/day9/Location.kt
@@ -1,30 +1,11 @@
 package advent.of.code.day9
 
-class Location(heights: List<List<Int>>, x: Int, y: Int) {
-    val height = heights[y][x]
-    val adjacentHeights: List<Int>
-    val isLowPoint: Boolean
-    val riskLevel: Int
-    val basinNumbers: List<Int> = listOf()
+class Location(val height: Int) {
+    val adjacentLocations = mutableListOf<Location>()
 
-    init {
-        val maxY = heights.size - 1
-        val maxX = heights[0].size - 1
-        val tmp = mutableListOf<Int>()
-        if (y < maxY) {
-            tmp.add(heights[y + 1][x])
-        }
-        if (y > 0) {
-            tmp.add(heights[y - 1][x])
-        }
-        if (x > 0) {
-            tmp.add(heights[y][x - 1])
-        }
-        if (x < maxX) {
-            tmp.add(heights[y][x + 1])
-        }
-        adjacentHeights = tmp.toList()
-        isLowPoint = adjacentHeights.all { it > height }
-        riskLevel = 1 + height
+    fun addAdjacentLocation(location: Location) {
+        adjacentLocations.add(location)
     }
+
+    fun isLowPoint() = adjacentLocations.all { it.height > height }
 }
